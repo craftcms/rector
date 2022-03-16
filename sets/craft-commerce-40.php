42467d11912824bf8cd4babc32b595b5ab2cd435
brandonkelly <brandon@pixelandtonic.com>
2022-03-16 16:09:48 -0700

    Add rules for the simple property/method renames in Commerce 4

--- sets/craft-commerce-40.php
+++ sets/craft-commerce-40.php
@@ -3,9 +3,25 @@
 declare(strict_types=1);
 
 use craft\rector\SignatureConfigurator;
+use Rector\Renaming\Rector\MethodCall\RenameMethodRector;
+use Rector\Renaming\Rector\PropertyFetch\RenamePropertyRector;
+use Rector\Renaming\ValueObject\MethodCallRename;
+use Rector\Renaming\ValueObject\RenameProperty;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function(ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+
+    $services->set(RenamePropertyRector::class)
+        ->configure([
+            new RenameProperty('craft\commerce\model\ProductType', 'titleFormat', 'variantTitleFormat'),
+        ]);
+
+    $services->set(RenameMethodRector::class)
+        ->configure([
+            new MethodCallRename('craft\commerce\services\ShippingMethods', 'getAvailableShippingMethods', 'getMatchingShippingMethods'),
+        ]);
+
     // Property/method signatures
     SignatureConfigurator::configure($containerConfigurator, 'craft-commerce-40');
 };
